de11b02ae7fbefd243d25def9c1eaed418d5cd73
KIBI312 <isken00.kg@gmail.com>
2023-04-23 18:02:31 +0600

    Init

---
+++ src/test/java/com/seitov/vinylapi/VinylApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seitov.vinylapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
