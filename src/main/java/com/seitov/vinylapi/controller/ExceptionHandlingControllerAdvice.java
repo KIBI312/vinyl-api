8e2b69eb3fd161ef8b284a088d71621603eabf7c
KIBI312 <isken00.kg@gmail.com>
2023-05-10 16:55:22 +0600

    Adds exception handler for missing request params

--- src/main/java/com/seitov/vinylapi/controller/ExceptionHandlingControllerAdvice.java
+++ src/main/java/com/seitov/vinylapi/controller/ExceptionHandlingControllerAdvice.java
@@ -4,6 +4,7 @@ import com.seitov.vinylapi.dto.ResponseMessage;
 import com.seitov.vinylapi.exception.ResourceNotFoundException;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,5 +19,11 @@ public class ExceptionHandlingControllerAdvice {
         return new ResponseMessage(404, "NOT_EXIST", ex.getMessage());
     }
 
+    @Hidden
+    @ExceptionHandler(value = {MissingServletRequestParameterException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseMessage missingParameter(MissingServletRequestParameterException ex) {
+        return new ResponseMessage(400, "MISSING_PARAMETER", ex.getMessage());
+    }
 
 }
