b71beb77928338116632ec96d2a92721f4193d73
KIBI312 <isken00.kg@gmail.com>
2023-05-10 16:54:54 +0600

    Adds generalized method for dynamic projection usage

--- src/main/java/com/seitov/vinylapi/repository/ArtistRepository.java
+++ src/main/java/com/seitov/vinylapi/repository/ArtistRepository.java
@@ -1,10 +1,15 @@
 package com.seitov.vinylapi.repository;
 
 import com.seitov.vinylapi.entity.Artist;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
+
+    <T> List<T> findAllProjectedBy(Pageable pageable, Class<T> type);
 
 }
