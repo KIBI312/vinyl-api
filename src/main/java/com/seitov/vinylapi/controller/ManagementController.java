7a7f9f768e473c34a5f542eca020d37fc5f99f94
KIBI312 <isken00.kg@gmail.com>
2023-05-15 21:10:43 +0600

    Adds explicit swagger documentation for genre deletion mapping

--- src/main/java/com/seitov/vinylapi/controller/ManagementController.java
+++ src/main/java/com/seitov/vinylapi/controller/ManagementController.java
@@ -37,6 +37,16 @@ public class ManagementController {
     }
 
     @Operation(description = "Deletes genre by id", tags = "management")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content =
+            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = ResponseMessage.class)))),
+            @ApiResponse(responseCode = "404", description = "Trying to delete non-existing genre",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ResponseMessage.class))),
+            @ApiResponse(responseCode = "409", description = "Trying to delete genre with dependent vinyls",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ResponseMessage.class)))})
     @DeleteMapping("/genres")
     public ResponseMessage deleteGenre(@RequestBody ResourceId genreId) {
         managementService.deleteGenre(genreId);
