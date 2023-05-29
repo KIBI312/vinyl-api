6b167b11d150c61c07df509cbdc55f5ad1a7d25b
KIBI312 <isken00.kg@gmail.com>
2023-05-29 18:58:25 +0600

    Replaces ArtistName projection with ArtistShort entity

--- src/main/java/com/seitov/vinylapi/dto/VinylDto.java
+++ src/main/java/com/seitov/vinylapi/dto/VinylDto.java
@@ -1,8 +1,8 @@
 package com.seitov.vinylapi.dto;
 
+import com.seitov.vinylapi.entity.ArtistShort;
 import com.seitov.vinylapi.entity.Format;
 import com.seitov.vinylapi.entity.Genre;
-import com.seitov.vinylapi.projection.ArtistName;
 import lombok.Data;
 
 import java.util.List;
@@ -14,7 +14,7 @@ public class VinylDto {
     private String name;
     private String description;
     private Double price;
-    private List<ArtistName> artists;
+    private List<ArtistShort> artists;
     private List<Genre> genres;
     private Format format;
     private Boolean inStock;
