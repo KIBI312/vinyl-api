95b60d1a566bed7fd36616c9246083158f4ee2e6
KIBI312 <isken00.kg@gmail.com>
2023-05-30 20:09:36 +0600

    Overrides hashCode&equals for value check instead reference check

--- src/main/java/com/seitov/vinylapi/entity/ArtistShort.java
+++ src/main/java/com/seitov/vinylapi/entity/ArtistShort.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.Immutable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Immutable
@@ -21,5 +22,18 @@ public class ArtistShort {
     @Id
     private Long id;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArtistShort that = (ArtistShort) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 
 }
